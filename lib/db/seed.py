6952b58b1e7e2b1b06aace663f137fd5e0d842e9
Barasa <timbaros87@gmail.com>
2024-07-30 00:08:12 +0300

    Create categories

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -6,3 +6,11 @@ engine = create_engine('sqlite:///expsense.db') #engine that connectes to the SQ
 Session = sessionmaker(bind=engine) #a configured Session class
 session = Session() #session instance
 
+# Create categories
+categories = [
+    Category(name="Food"), 
+    Category(name="Transporatation"), 
+    Category(name="Utilities")
+    ]
+session.add_all(categories)
+session.commit()
