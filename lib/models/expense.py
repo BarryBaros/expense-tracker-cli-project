b11601edf8f8df1f66fd1a28ddf29112d0c0b5a7
Barasa <timbaros87@gmail.com>
2024-07-29 22:53:17 +0300

    Define the Category Class

--- lib/models/expense.py
+++ lib/models/expense.py
@@ -5,3 +5,14 @@ from . import Base
 # Define the Expense class to map the to table in the database
 class Expense(Base):
     __tablename__ = 'expenses' # Table name in the database
+
+# Class attirbutes
+    id = Column(Integer, primary_key=True)  #Primary Key
+    amount = Column(Float, nullable=False)  #Amount of expenses, cannot be null
+    category_id = Column(Integer, ForeignKey('categories.id'))  #Foreign Key
+    date = Column(Date, nullable=False) #Date of the expense, cannot be null
+
+    category = relationship("Category", back_populates="expenses")
+
+    def __repr__(self):
+        return f"<Expense(id={self.id}, amount={self.amount}, date={self.date})>"
