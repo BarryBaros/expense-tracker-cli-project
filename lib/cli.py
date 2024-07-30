db7363bae74cff3a046450350159eb6c08d9043a
Barasa <timbaros87@gmail.com>
2024-07-30 04:06:00 +0300

    Add new expenses

--- lib/cli.py
+++ lib/cli.py
@@ -1,28 +1,14 @@
 # lib/cli.py
+import click
+from lib.helpers import create_expense, list_expenses, create_category, list_categories, create_budget, list_budgets, generate_report
 
-from helpers import (
-    exit_program,
-    helper_1
-)
+@click.group()
+def cli():
+    pass
 
-
-def main():
-    while True:
-        menu()
-        choice = input("> ")
-        if choice == "0":
-            exit_program()
-        elif choice == "1":
-            helper_1()
-        else:
-            print("Invalid choice")
-
-
-def menu():
-    print("Please select an option:")
-    print("0. Exit the program")
-    print("1. Some useful function")
-
-
-if __name__ == "__main__":
-    main()
+@cli.command()
+@click.option('--amount', prompt='Expense amount',  type=float)
+@click.option('--category', prompt='Expense category')
+@click.option('--date', prompt='Expense date (YYYY, MM, DD)')
+def add_expense(amount, category, date):
+    create_expense(amount, category, date)
